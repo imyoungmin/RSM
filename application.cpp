a7f27ceb8a15bb35cbee8f4b51ea898320b771e6
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2019-03-02 14:58:42 -0800

    Added Xcode project.  Changed configuration.  Made window non resizable.

--- application.cpp
+++ application.cpp
@@ -322,6 +322,7 @@ int main( int argc, const char * argv[] )
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+	glfwWindowHint( GLFW_RESIZABLE, false );
 	
 	cout << glfwGetVersionString() << endl;
 
